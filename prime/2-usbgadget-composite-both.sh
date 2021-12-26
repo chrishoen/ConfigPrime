f809f0280b88cfaa8eb51f5ddf8dd61ecf880b2f
chrishoen <chris123hoen.gmail.com>
2021-12-26 17:35:51 -0500

    more usb

--- prime/2-usbgadget-composite-both.sh
+++ prime/2-usbgadget-composite-both.sh
@@ -19,12 +19,12 @@ mkdir configs/c.1/strings/0x409
 echo "my configuration" > configs/c.1/strings/0x409/configuration
 
 # create function
-mkdir functions/acm.usb0
-ln -s functions/acm.usb0 configs/c.1
-
-# create function
 mkdir functions/gser.usb0
 ln -s functions/gser.usb0 configs/c.1
+
+# create function
+mkdir functions/acm.usb0
+ln -s functions/acm.usb0 configs/c.1
 
 # activate
 ls /sys/class/udc > UDC
